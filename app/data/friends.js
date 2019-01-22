8b5642f8b2664aee2e571671c34158e3647a661e
matts112 <mttswinford@yahoo.com>
2019-01-22 01:04:25 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,115 @@
+var friends = [{
+    "name": "Pickachu",
+    "photo": 'https://vignette.wikia.nocookie.net/wii/images/8/89/Pikachu.jpg/revision/latest?cb=20140209205851',
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Bulbasaur",
+    "photo": 'https://vignette.wikia.nocookie.net/central/images/5/51/Bulbasaur.jpg/revision/latest?cb=20120606020155',
+    "scores": [
+        2,
+        4,
+        5,
+        2,
+        3,
+        1,
+        3,
+        5,
+        1,
+        4
+    ]
+},
+{
+    "name": "Charmander",
+    "photo": 'https://vignette.wikia.nocookie.net/pokemoncrater/images/f/f2/Charmander.jpg/revision/latest?cb=20090929100232',
+    "scores": [
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2,
+        3,
+        2
+    ]
+},
+{
+    "name": "Squirtle",
+    "photo": 'https://vignette.wikia.nocookie.net/pokemon-planet/images/f/f3/Squirtle.jpg/revision/latest?cb=20171102155659',
+    "scores": [
+        2,
+        2,
+        2,
+        3,
+        4,
+        3,
+        4,
+        5,
+        1,
+        1
+    ]
+},
+{
+    "name": "Larvatar",
+    "photo": 'https://vignette.wikia.nocookie.net/pokemontowerdefense/images/2/2d/Larvitar.jpg/revision/latest?cb=20131022220943',
+    "scores": [
+        1,
+        1,
+        4,
+        4,
+        2,
+        2,
+        4,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Chimchar",
+    "photo": 'https://cdn.bulbagarden.net/upload/7/76/390Chimchar.png',
+    "scores": [
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        3,
+        3,
+        2,
+        2
+    ]
+},
+{
+    "name": "Espeon",
+    "photo": 'https://vignette.wikia.nocookie.net/pal-series/images/b/b3/Espeon.jpg/revision/latest?cb=20160107030631',
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2
+    ]
+}
+]
+
+module.exports = friends;
